f12baa2338b8b099645697e43c0d6db62d5ff484
qusuyan <qusuyan@gmail.com>
2025-12-04 21:27:35 -0600

    record per txn storage access time

--- core/state_processor.go
+++ core/state_processor.go
@@ -115,7 +115,9 @@ func (p *StateProcessor) Process(block *types.Block, parent *types.Header, state
 		receipts = append(receipts, receipt)
 		allLogs = append(allLogs, receipt.Logs...)
 		txTime := time.Since(txStart)
-		p.logger.Printf("%d,%x,%d,%d,%.5f", block.Number(), i, msg.GasLimit, receipt.GasUsed, txTime.Seconds()*1000)
+		txTimeMillis := txTime.Seconds() * 1000
+		storageReadTimeMillis := statedb.StorageReads.Seconds() * 1000
+		p.logger.Printf("%d,%x,%d,%d,%.5f,%.5f", block.Number(), i, msg.GasLimit, receipt.GasUsed, txTimeMillis, storageReadTimeMillis)
 	}
 	// Finalize the block, applying any consensus engine specific extras (e.g. block rewards)
 	if err := p.engine.Finalize(p.bc, block, parent, statedb, receipts); err != nil {
